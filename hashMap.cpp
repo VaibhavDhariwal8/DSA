5b2ed8c327245dbb77a23104a1a8e505899d17a3
VaibhavDhariwal8 <vaibhavdhariwal8@gmail.com>
2024-09-14 14:38:24 +0000

    #Optimising through unordered map

--- hashMap.cpp
+++ hashMap.cpp
@@ -6,15 +6,16 @@ int main()
     int n;
     cin>>n;
     int arr[n];
+    unordered_map<int,int> mpp;
     for(int i=0;i<n;i++){
         cin >> arr[i];
-    }
-
-    // pre compute
-    map<int,int> mpp;
-    for(int i=0;i<n;i++){
         mpp[arr[i]]++;
     }
+
+    // iterate in the map
+    // for(auto it:mpp){
+    //     cout << it.first << "->"<< it.second << endl;
+    // }
 
     int q;
     cin >>q;
@@ -26,3 +27,6 @@ int main()
     }
     return 0;
 }
+
+// unordered_map O(1) -> avg and best case  O(N) -> worst case
+// map O(log(N)) -> avg best and worst case
